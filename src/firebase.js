c21c02dcefa75518245c3c3bcea35cccffc4e175
wjswlgns03 <jjh030726@tukorea.ac.kr>
2023-09-03 12:45:33 +0900

    1.2

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD-gYE2CsoasfUUjOd4iKmJeYEMlmOL-0s",
+  authDomain: "nwitter-11de2.firebaseapp.com",
+  projectId: "nwitter-11de2",
+  storageBucket: "nwitter-11de2.appspot.com",
+  messagingSenderId: "587272915475",
+  appId: "1:587272915475:web:2a52f467c6b8aebfc34de6",
+};
+
+export default firebase.initializeApp(firebaseConfig);
