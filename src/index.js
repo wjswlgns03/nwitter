c11ffbe56911da25c7c4164af03da873ea2bc0b1
wjswlgns03 <jjh030726@tukorea.ac.kr>
2023-09-03 13:00:35 +0900

    1.3

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import App from "./App";
 import firebase from "./firebase";
 console.log(firebase);
